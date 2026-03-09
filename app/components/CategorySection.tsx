eb31fed1bc7f1568d0f9bc6ee7216a829bbe3839
bruhkookie <calebwongtech@gmail.com>
2026-03-09 01:41:57 -0700

    get ready to delopy

--- app/components/CategorySection.tsx
+++ app/components/CategorySection.tsx
@@ -11,7 +11,7 @@ interface CategorySectionProps {
   categoryPct: number | null;
   finalExamScore?: string;
   useFinal?: boolean;
-  onFinalScoreChange?: (value: string) => void;
+  onFinalScoreChange?: (value: string) => void; //test
   onUseFinalChange?: (checked: boolean) => void;
 }
 
